605a3ab5ad9e77f0374cee386aad7c57276487d9
Isaac <cookeisaac2@gmail.com>
2023-04-11 15:17:21 +0100

    Initial Commit

---
+++ cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/tmp/PioasmBuild-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/SDKs/pico/pico-sdk/tools/pioasm"
+  "D:/SDKs/pico/test/cmake-build-release/pioasm"
+  "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm"
+  "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/tmp"
+  "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp"
+  "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src"
+  "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/SDKs/pico/test/cmake-build-release/pico-sdk/src/rp2_common/pico_cyw43_driver/pioasm/src/PioasmBuild-stamp${cfgdir}") # cfgdir has leading slash
+endif()
